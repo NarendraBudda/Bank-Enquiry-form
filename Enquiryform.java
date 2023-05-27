693e00b9dd9801aff7ddd86bc62ef82978c561f1
Buddareddy Narendra Reddy <87933304+NarendraBudda@users.noreply.github.com>
2023-05-27 10:47:13 +0530

    Add files via upload

---
+++ Enquiryform.java
@@ -0,0 +1,22 @@
+class Enquiryform {
+	public static void main(String[] args){
+		String name = "John";
+		long contactnumber = 412312556l;
+		String email = "john@gmail.com";
+		int age = 34;
+		char gender = 'm';
+		String occupation = "manager";
+		double annualsalary = 10000;
+		String nationality = "indian";
+		boolean ismarried = true;
+		System.out.println("account holder name is "+name);
+		System.out.println("account holder  contact number is "+contactnumber);
+		System.out.println("account holder email is "+email);
+		System.out.println("account holder age is "+age);
+		System.out.println("account holder gender is "+gender);
+		System.out.println("account holder occupation is "+occupation);
+		System.out.println("account holder annual salary is "+annualsalary);
+		System.out.println("account holder nationality is "+nationality);
+		System.out.println("account holder is married "+ismarried);
+	}
+}
